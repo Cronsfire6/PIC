0473c678599146901c7fcaa3b6de3762c8ff08e4
Cronsfire6 <riosvictor.eo@gmail.com>
2022-03-21 20:46:40 -0300

    Inicio

---
+++ Pt100/PT100.X/build/default/production/main.p1.d
@@ -0,0 +1,14 @@
+ build/default/production/main.d  \
+ build/default/production/main.p1:  \
+ main.c  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/language_support.h  \
+configuracion.h  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/errata.h  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/c90/stdint.h  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/__at.h  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/proc/pic18f4431.h  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/htc.h  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/pic18_chip_select.h  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/xc.h  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/pic18.h  \
+../../../../../../../Program\ Files/Microchip/xc8/v2.32/pic/include/builtins.h 
---
+++ Pt100/PT100.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: Usuario
+ *
+ * Created on 19 de marzo de 2022, 12:26
+ */
+#include "configuracion.h"
+
+void main(void) {
+    
+    OSCCONbits.IRCF=111;
+    return;
+}
